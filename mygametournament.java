4d85d9576d78706870036f53035956dba4e34a65
Adarsh Kshirsagar <116270280+adarshksagar17@users.noreply.github.com>
2023-10-23 06:05:31 -0700

    Update mygametournament.java

--- mygametournament.java
+++ mygametournament.java
@@ -1,6 +1,6 @@
-package project;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 class Player {
     String name;
@@ -23,19 +23,26 @@ class Game {
     }
 
     void playGame() {
-        String[] moves = {"rock", "scissors"};
+        String[] moves = {"rock", "paper", "scissors"};
         Random random = new Random();
-        int move1 = random.nextInt(2);
-        int move2 = random.nextInt(2);
+        int move1 = random.nextInt(3);
+        int move2 = random.nextInt(3);
 
         result = determineWinner(moves[move1], moves[move2]);
+
+        if (result.equals(player1.name)) {
+            player1.score++;
+        } else if (result.equals(player2.name)) {
+            player2.score++;
+        }
     }
 
     String determineWinner(String move1, String move2) {
         if (move1.equals(move2)) {
             return "draw";
         } else if ((move1.equals("rock") && move2.equals("scissors")) ||
-                (move1.equals("scissors") && move2.equals("rock")) {
+                   (move1.equals("scissors") && move2.equals("paper")) ||
+                   (move1.equals("paper") && move2.equals("rock"))) {
             return player1.name;
         } else {
             return player2.name;
@@ -52,18 +59,18 @@ public class TournamentManager {
         manager.addPlayer("Player 3");
         manager.addPlayer("Player 4");
 
-        manager.createTournamentTree();
+        manager.createGames();
         manager.playTournament();
         manager.printLeaderboard();
     }
 
     List<Player> players;
-    List<Game> tournamentTree;
+    List<Game> games;
     List<Player> leaderboard;
 
     TournamentManager() {
         players = new ArrayList<>();
-        tournamentTree = new ArrayList<>();
+        games = new ArrayList<>();
         leaderboard = new ArrayList<>();
     }
 
@@ -71,49 +78,32 @@ public class TournamentManager {
         players.add(new Player(playerName));
     }
 
-    void createTournamentTree() {
-        if (players.size() < 2) {
-            System.out.println("The tournament requires at least 2 players.");
+    void createGames() {
+        if (players.size() % 2 != 0) {
+            System.out.println("The number of players must be even for tournament games.");
             return;
         }
 
-        for (int i = 0; i < players.size() - 1; i += 2) {
+        for (int i = 0; i < players.size(); i += 2) {
             Game game = new Game(players.get(i), players.get(i + 1));
-            tournamentTree.add(game);
+            games.add(game);
         }
     }
 
     void playTournament() {
-        while (tournamentTree.size() > 1) {
-            List<Game> nextRound = new ArrayList<>();
-            for (int i = 0; i < tournamentTree.size(); i += 2) {
-                Game game = new Game(tournamentTree.get(i).player1, tournamentTree.get(i + 1).player2);
-                game.playGame();
-                String result = game.result;
-
-                if (!result.equals("draw")) {
-                    Player winner = players.stream()
-                            .filter(player -> player.name.equals(result))
-                            .findFirst()
-                            .orElse(null);
-
-                    if (winner != null) {
-                        winner.score++;
-                    }
-                }
-
-                nextRound.add(game);
-            }
-            tournamentTree = nextRound;
+        for (Game game : games) {
+            game.playGame();
         }
+
+        leaderboard.clear();
+        leaderboard.addAll(players);
+        leaderboard.sort((p1, p2) -> Integer.compare(p2.score, p1.score));
     }
 
     void printLeaderboard() {
-        players.sort((p1, p2) -> Integer.compare(p2.score, p1.score));
-
         System.out.println("Tournament Leaderboard:");
-        for (int i = 0; i < players.size(); i++) {
-            System.out.println((i + 1) + ". " + players.get(i).name + " - Score: " + players.get(i).score);
+        for (int i = 0; i < leaderboard.size(); i++) {
+            System.out.println((i + 1) + ". " + leaderboard.get(i).name + " - Score: " + leaderboard.get(i).score);
         }
     }
 }
